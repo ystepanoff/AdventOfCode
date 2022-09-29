b9d998107508ae693c7d899e5b62e7324e76f988
ystepanoff <ystepanoff@icloud.com>
2022-09-29 11:56:05 +0100

    PE 209 solved

--- projecteuler/0209/p209.py
+++ projecteuler/0209/p209.py
@@ -2,6 +2,11 @@ def f(n):
     b = format(n, '06b')
     return int(b[1:] + str(int(b[0]) ^ (int(b[1]) & int(b[2]))), 2)
 
+
+c = [0, 1, 3]
+for i in range(3, 100):
+    c.append(c[i - 1] + c[i - 2])
+result = 1
 used = set()
 for i in range(2 ** 6):
     if i in used:
@@ -13,4 +18,5 @@ for i in range(2 ** 6):
         used.add(next)
         l += 1
         curr, next = next, f(next)
-    print(l)
+    result *= c[l]
+print(result)
