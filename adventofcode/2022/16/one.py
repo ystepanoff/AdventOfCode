e0e74a7903007ae2078764b409979ea36f48416f
Yegor Stepanov <ystepanoff@icloud.com>
2022-12-18 01:05:08 +0000

    AoC 2022 Day 16

---
+++ adventofcode/2022/16/one.py
@@ -0,0 +1,31 @@
+import re
+pattern = re.compile(r'Valve (.*) has flow rate=(\d*); tunnels? leads? to valves? (.+)')
+G = {}
+for line in open('input'):
+    line = line.rstrip()
+    match = pattern.match(line)
+    v = match.group(1)
+    r = int(match.group(2))
+    ch = match.group(3).split(', ')
+    G[v] = {
+        'rate': r,
+        'children': ch,
+    }
+
+CACHE = {}
+def dp(v='AA', opened=set(), time=30):
+    global CACHE, G
+    if time == 0:
+        return 0
+    key = (v, str(opened), time)
+    if key in CACHE:
+        return CACHE[key]
+    result = 0
+    for ch in G[v]['children']:
+        result = max(result, dp(ch, opened, time - 1))
+    if v not in opened and G[v]['rate'] > 0:
+        result = max(result, (time - 1) * G[v]['rate'] + dp(v, opened.union({v}), time - 1))
+    CACHE[key] = result
+    return result
+
+print(dp())
