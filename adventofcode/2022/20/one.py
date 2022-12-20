bd6bf266f59b8691bc3e886e2ce589ebdea239b9
ystepanoff <ystepanoff@icloud.com>
2022-12-20 11:42:59 +0000

    AoC 2022 Day 20

---
+++ adventofcode/2022/20/one.py
@@ -0,0 +1,14 @@
+from collections import deque
+a = [int(line.rstrip()) for line in open('input') if line.rstrip()]
+a = deque(list(enumerate(a)))
+for i in range(len(a)):
+    while a[0][0] != i:
+        a.append(a.popleft())
+    v = a.popleft()
+    for _ in range(v[1] % len(a)):
+        a.append(a.popleft())
+    a.append(v)
+a = [y for _, y in a]
+idx = a.index(0)
+print(a[(idx + 1000) % len(a)] + a[(idx + 2000) %
+      len(a)] + a[(idx + 3000) % len(a)])
