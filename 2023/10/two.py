dd5895173eef1e3c5d00bae54e1cc055fc18947d
Yegor Stepanov <ystepanoff@icloud.com>
2023-12-11 21:25:53 +0000

    AoC 2023 Day 10

---
+++ 2023/10/two.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+import sys
+
+LEFT = (0, -1)
+RIGHT = (0, 1)
+UP = (-1, 0)
+DOWN = (1, 0)
+
+MATCH = {
+    LEFT: "-FL",
+    RIGHT: "-J7",
+    UP: "|7F",
+    DOWN: "|JL",
+}
+
+def check(ch1, ch2, d):
+    dirs = None
+    if ch2 == 'S':
+        return True
+    match ch1:
+        case '|':
+            dirs = [UP, DOWN]
+        case '-':
+            dirs = [LEFT, RIGHT]
+        case 'L':
+            dirs = [UP, RIGHT]
+        case 'J':
+            dirs = [UP, LEFT]
+        case '7':
+            dirs = [DOWN, LEFT]
+        case 'F':
+            dirs = [DOWN, RIGHT]
+        case 'S':
+            dirs = [UP, DOWN, LEFT, RIGHT]
+    return d in dirs and ch2 in MATCH[d]
+
+grid = []
+for line in sys.stdin:
+    grid.append(list(line.rstrip()))
+
+n, m = len(grid), len(grid[0])
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == 'S':
+            xs, ys = i, j
+
+path = [(xs, ys)]
+while True:
+    if len(path) > 1 and path[-1] == (xs, ys):
+        break
+    x, y = path[-1]
+    for dx, dy in [LEFT, RIGHT, UP, DOWN]:
+        xx, yy = x + dx, y + dy
+        if 0 <= xx < n and 0 <= yy < m:
+            if check(grid[x][y], grid[xx][yy], (dx, dy)) and not (xx, yy) in path:
+                path.append((xx, yy))
+                break
+    else:
+        break
+
+
+from matplotlib.path import Path # cheeky, otherwise a standard computational geometry problem :)
+p = Path(path)
+total = 0
+for x in range(n):
+    for y in range(m):
+        if p.contains_point((x, y)) and not (x, y) in path:
+            total += 1
+print(total)
