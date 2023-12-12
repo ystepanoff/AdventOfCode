aecf26c45564567dc70054cd14ba4ac140413290
Yegor Stepanov <ystepanoff@icloud.com>
2023-12-12 13:03:13 +0000

    AoC 2023 Day 12

--- 2023/12/two.py
+++ 2023/12/two.py
@@ -1,9 +1,11 @@
 import sys
 
+
 CACHE = {}
+
+
 def dp(arr, groups, i, j, curr):
     global CACHE
-    
     key = (i, j, curr)
     if key in CACHE:
         return CACHE[key]
@@ -15,26 +17,30 @@ def dp(arr, groups, i, j, curr):
         else:
             return 0
     total = 0
-    for c in '.#':
-        if arr[i] in [c, '?']:
-            if c == '.' and curr == 0:
-                total += dp(arr, groups, i + 1, j, 0)
-            elif c == '.' and curr > 0 and j < len(groups) and groups[j] == curr:
-                total += dp(arr, groups, i + 1, j + 1, 0)
-            elif c == '#':
-                total += dp(arr, groups, i + 1, j, curr + 1)
+    if arr[i] == '.':
+        if curr == 0:
+            total += dp(arr, groups, i + 1, j, 0)
+        elif j < len(groups) and groups[j] == curr:
+            total += dp(arr, groups, i + 1, j + 1, 0)
+    elif arr[i] == '#':
+        total += dp(arr, groups, i + 1, j, curr + 1)
+    elif arr[i] == '?':
+        total += dp(arr, groups, i + 1, j, curr + 1)
+        if curr == 0:
+            total += dp(arr, groups, i + 1, j, 0)
+        elif j < len(groups) and groups[j] == curr:
+            total += dp(arr, groups, i + 1, j + 1, 0)
     CACHE[key] = total
     return total
-        
+
 
 total = 0
 for line in sys.stdin:
-    line = line.rstrip()
-    arr, groups = line.split()
+    arr, groups = line.rstrip().split()
     arr = '?'.join([arr] * 5)
     groups = ','.join([groups] * 5)
     arr = list(arr)
     groups = [int(g) for g in groups.split(',')]
-    CACHE = {}
+    CACHE.clear()
     total += dp(arr, groups, 0, 0, 0)
 print(total)
