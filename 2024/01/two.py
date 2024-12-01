c4171fc2373c063652fcfd31c0a90d9296eec59a
Yegor Stepanov <ystepanoff@icloud.com>
2024-12-01 05:08:47 +0000

    AoC 2024 Day 1

---
+++ 2024/01/two.py
@@ -0,0 +1,18 @@
+import sys
+
+a, b = [], []
+for line in sys.stdin:
+	x, y = map(int, line.split())
+	a.append(x)
+	b.append(y)
+
+a.sort()
+b.sort()
+
+print(a, b)
+
+total = 0
+for x in a:
+	total += b.count(x) * x
+
+print(total)
