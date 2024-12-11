f33f1076902c4df38d2adde001564d63ee708a05
Yegor Stepanov <ystepanoff@icloud.com>
2024-12-11 00:36:30 -0500

    AoC 2024 Day 11

---
+++ 2024/11/two.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+
+def count(st):
+    s = Counter({st: 1})
+    for _ in range(75):
+        new_s = Counter()
+        for stone, cnt in s.items():
+            str_stone = str(stone)
+            l = len(str_stone)
+            if stone == 0:
+                new_s[1] += cnt
+            elif l % 2 == 0:
+                mid = l // 2
+                new_s[int(str_stone[:mid])] += cnt
+                new_s[int(str_stone[mid:])] += cnt
+            else:
+                new_s[stone * 2024] += cnt
+        s = new_s
+    return sum(s.values())
+
+
+print(sum(count(int(s)) for s in input().split()))
