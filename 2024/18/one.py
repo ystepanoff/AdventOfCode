6ce8a5a54fcd4c0fffb3e97599a3a72b35383c5d
Yegor Stepanov <ystepanoff@icloud.com>
2024-12-18 05:43:57 +0000

    AoC 2024 Day 18

--- 2024/18/one.py
+++ 2024/18/one.py
@@ -7,10 +7,10 @@ G = G - set(inp[:1024])
 q = [0]
 dist = {0: 0}
 while q:
-	u = q.pop(0)
-	for i in range(4):
-		v = u + (-1j) ** i
-		if v in G and v not in dist:
-			dist[v] = dist[u] + 1
-			q.append(v)
+    u = q.pop(0)
+    for i in range(4):
+        v = u + (-1j) ** i
+        if v in G and v not in dist:
+            dist[v] = dist[u] + 1
+            q.append(v)
 print(dist[70 + 70j])
